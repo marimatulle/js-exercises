3f6715e585f64feea93a30e171a387be52ccab7b
Marina Matulle <marinamatulle.mm@gmail.com>
2023-08-10 14:54:24 -0300

    refactored codes

--- parseLogs.js
+++ parseLogs.js
@@ -1,25 +1,45 @@
+const calculateTotalSales = (qt, price) => {
+  const total = qt * price;
+  const valueInReal = total / 100;
+  return `R$${valueInReal.toFixed(2).replace(".", ",")}`;
+};
+
+const findExistingSale = (sales, id) => {
+  return sales.find((sale) => sale.id === id);
+};
+
+const addProductToSale = (sale, product) => {
+  sale.products.push(product);
+};
+
+const createNewSale = (transaction, product) => {
+  return {
+    id: transaction.id,
+    products: [
+      {
+        name: product.name,
+        total: calculateTotalSales(transaction.qt, product.price),
+      },
+    ],
+  };
+};
+
 const parseLog = (logs) => {
   const sales = [];
+
   logs.forEach((product) => {
     product.transactions.forEach((transaction) => {
-      const totalSales = transaction.qt * product.price;
-      const existingSale = sales.find((sale) => sale.id === transaction.id);
+      const totalSales = calculateTotalSales(transaction.qt, product.price);
+      const existingSale = findExistingSale(sales, transaction.id);
 
       if (existingSale) {
-        existingSale.products.push({
+        addProductToSale(existingSale, {
           name: product.name,
           total: totalSales,
         });
       } else {
-        sales.push({
-          id: transaction.id,
-          products: [
-            {
-              name: product.name,
-              total: totalSales,
-            },
-          ],
-        });
+        const newSale = createNewSale(transaction, product);
+        sales.push(newSale);
       }
     });
   });
--- parseLogs.test.js
+++ parseLogs.test.js
@@ -9,55 +9,55 @@ describe('parseLogs', () => {
                 {
                 id: '001',
                 products: [
-                    { name: 'apple', total: 600 },
-                    { name: 'orange', total: 250 },
-                    { name: 'rice', total: 1000},
-                    { name: 'soap', total: 250 }
+                    { name: 'apple', total: "R$6,00" },
+                    { name: 'orange', total: "R$2,50" },
+                    { name: 'rice', total: "R$10,00" },
+                    { name: 'soap', total: "R$2,50" }
                 ],
             },
             {
                 id: '002',
                 products: [
-                    { name: 'apple', total: 200 },
-                    { name: 'orange', total: 250 },
-                    { name: 'rice', total: 500 }
+                    { name: 'apple', total: "R$2,00" },
+                    { name: 'orange', total: "R$2,50" },
+                    { name: 'rice', total: "R$5,00" }
                 ],
             },
             {
                 id: '003',
                 products: [
-                    { name: 'apple', total: 7400 },
-                    { name: 'chocolat', total: 13500 },
-                    { name: 'soap', total: 100 }
+                    { name: 'apple', total: "R$74,00" },
+                    { name: 'chocolat', total: "R$135,00" },
+                    { name: 'soap', total: "R$1,00" }
                 ],
             },
             {
                 id: '004',
                 products: [
-                    { name: 'apple', total: 1000 },
-                    { name: 'milk', total: 10800 },
-                    { name: 'soap', total: 50 }
+                    { name: 'apple', total: "R$10,00" },
+                    { name: 'milk', total: "R$108,00" },
+                    { name: 'soap', total: "R$0,50" }
                 ],
             },
             {
                 id: '005',
                 products: [
-                    { name: 'milk', total: 3600 },
-                    { name: 'rice', total: 1500 },
+                    { name: 'milk', total: "R$36,00" },
+                    { name: 'rice', total: "R$15,00" },
                 ],
             },
             {
                 id: '006',
                 products: [
-                    { name: 'milk', total: 4500 },
-                    { name: 'chocolat', total: 1500 },
+                    { name: 'milk', total: "R$45,00" },
+                    { name: 'chocolat', total: "R$15,00" },
                 ],
             },
             {
                 id: '007',
                 products: [
-                    { name: 'milk', total: 8100 },
-                    { name: 'chocolat', total: 7500 }
+                    { name: 'milk', total: "R$81,00" },
+                    { name: 'chocolat', total: "R$75,00" }
                 ],
             },
             ]
